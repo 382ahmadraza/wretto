587650670f79adabacd623679c47846ac9f9b303
Ahmad Raza <382ahmadraza@gmail.com>
2025-05-23 10:29:10 +0500

    wretto

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./(pages)/home/Home"; 
+
+
+function App() {
+  return (
+    <div className="App ">
+      <Router>
+          <Routes>
+              <Route path="/" element={<Home/>}></Route>
+          </Routes>
+      </Router>
+      
+    </div>
+  );
+}
+
+export default App;
